f6050a9de1f432d8d25d24666cafe4495cece1df
SaraMsoso <Sara.soder44@gmail.com>
2025-12-01 09:04:19 +0100

    Added Vue app setup, router registration and global styles

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import './style.css'
 
+// Create Vue application, mount router, and mount the app to index.html (#app)
 createApp(App)
   .use(router)
   .mount('#app')
