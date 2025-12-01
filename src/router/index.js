b43560b11124dcf3ef8c6c03ccc3e3a6418caaac
SaraMsoso <Sara.soder44@gmail.com>
2025-12-01 14:30:05 +0100

    Fixing URL 404 error from Vercel

--- src/router/index.js
+++ src/router/index.js
@@ -6,13 +6,32 @@ import Info from '../pages/Info.vue'
 
 // Define routes for the application where each route maps to a component
 const routes = [
-  { path: '/', component: Home },
-  { path: '/movies', component: Movies },
-  { path: '/info', component: Info }
+  {
+    path: "/",
+    component: Home,
+    meta: { title: "Startsida" }
+  },
+  {
+    path: "/movies",
+    component: Movies,
+    meta: { title: "Filmer" }
+  },
+  {
+    path: "/info",
+    component: Info,
+    meta: { title: "Information" }
+  }
 ]
 
 // Create and export the router instance 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes
 })
+
+// Dynamic set the document title based on the route meta title
+router.afterEach((to) => {
+  document.title = to.meta.title || "Laboration 3 - Vue"
+})
+
+export default router
