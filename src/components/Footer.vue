6fdeff2fbad5bd65487e40ea001de60c02559384
SaraMsoso <Sara.soder44@gmail.com>
2025-12-01 09:12:32 +0100

    Added styled Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="bg-black text-center py-6 border-t border-neutral-800">
+    <p class="text-xl text-[#ff1d28]" style="font-family: var(--font-serif);">&copy; 2025 Sara Mohammad – Laboration 3</p>
+  </footer>
+</template>
